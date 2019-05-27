601abe925b4492fbaa23674609a8d465bdea11b8
Thành Phát IT <48637758+thanhphatit@users.noreply.github.com>
2019-05-27 15:01:55 +0700

    Upload

--- get-link.js
+++ get-link.js
@@ -1,4 +1,1 @@
-var link1 = "https://www.google.com/",
-    link2 = "Link tải 2",
-    link3 = "Link tải 3",
-    linkn = "Link tải n";
+var link1 = 'https://www.google.com/',link2 = "Link tải 2",link3 = "Link tải 3",linkn = "Link tải n";
