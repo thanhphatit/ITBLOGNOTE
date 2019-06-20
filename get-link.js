568eb757ee6486c63157b072cecfafa8f6d44612
Thành Phát IT <48637758+thanhphatit@users.noreply.github.com>
2019-06-20 18:17:33 +0700

    Update get-link.js

--- get-link.js
+++ get-link.js
@@ -1,4 +1,4 @@
-var link1 = "Link tải 1",
-    link2 = "Link tải 2",
-    link3 = "Link tải 3",
+var link1 = "Link tải 1";
+    link2 = "Link tải 2";
+    link3 = "Link tải 3";
     linkn = "Link tải n";
